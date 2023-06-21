eb9c3c9e22ad8c737fdec17560bf0d0e45e34953
npogoncuk <61973127+npogoncuk@users.noreply.github.com>
2023-06-21 18:24:34 +0300

    Done homework1

--- AndroidGeneralOverview/app/src/main/java/com/example/androidgeneraloverview/MainActivity.kt
+++ AndroidGeneralOverview/app/src/main/java/com/example/androidgeneraloverview/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.androidgeneraloverview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appInfo = "${BuildConfig.BUILD_TYPE} ${BuildConfig.FLAVOR} ${BuildConfig.VERSION_NAME} ${BuildConfig.BASE_URL}"
+        Log.d("appInfo", appInfo)
     }
 }
