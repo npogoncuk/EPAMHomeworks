597200c03fb56a19e1fd397018cd29e8b224ef0e
npogoncuk <npogoncuk@gmail.com>
2023-07-18 15:30:45 +0300

    Create WebView

--- ViewsandLayoutsPart2/app/src/main/java/com/example/viewsandlayoutspart2/WebViewActivity.kt
+++ ViewsandLayoutsPart2/app/src/main/java/com/example/viewsandlayoutspart2/WebViewActivity.kt
@@ -2,6 +2,7 @@ package com.example.viewsandlayoutspart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.example.viewsandlayoutspart2.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
@@ -15,5 +16,19 @@ class WebViewActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarLayout.toolbar.apply {
             title = "WebView Task"
         })
+        if (savedInstanceState == null) {
+            with(binding.webView) {
+                webViewClient = WebViewClient()
+                loadUrl("https://developer.android.com")
+            }
+        } else {
+            binding.webView.restoreState(savedInstanceState)
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.webView.saveState(outState)
+    }
+
 }
