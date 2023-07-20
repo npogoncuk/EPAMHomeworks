64336fa00115ebb57ecd4e40e4fa88f9986fd9ab
npogoncuk <npogoncuk@gmail.com>
2023-07-20 16:50:04 +0300

    Create button to change app theme Create a separate style for this button

--- ThemeAndStyles/app/src/main/java/com/example/themeandstyles/MainActivity.kt
+++ ThemeAndStyles/app/src/main/java/com/example/themeandstyles/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.themeandstyles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.themeandstyles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonChangeTheme.setOnClickListener {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
