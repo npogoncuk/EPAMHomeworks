2506f88c7b1ca120e8877efe6396ecca2858f691
npogoncuk <npogoncuk@gmail.com>
2023-09-02 14:25:50 +0300

    Implemented logic for showing Toast every minute

--- MultithreadingPart2/app/src/main/java/com/example/multithreadingpart2/MainActivity.kt
+++ MultithreadingPart2/app/src/main/java/com/example/multithreadingpart2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.multithreadingpart2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.multithreadingpart2.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener { start() }
+        binding.buttonStop.setOnClickListener { WorkManager.getInstance(applicationContext).cancelAllWork() }
+    }
+
+    private fun start() {
+        val workRequest = PeriodicWorkRequestBuilder<ShowNotificationWorker>(15, TimeUnit.MINUTES).build()
+        WorkManager.getInstance(applicationContext).enqueue(workRequest)
     }
 }
